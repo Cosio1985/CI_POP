8c4358ea66d0b4639771e602b539875b790ae8d5
Alfredo Martinez <martinezcosio@gmail.com>
2026-08-01 09:07:18 -0700

    arreglando la rama para los descuentos 2

--- src/test/java/com/empresa/integrador_demo/domain/CalculadoraDescuentosTest.java
+++ src/test/java/com/empresa/integrador_demo/domain/CalculadoraDescuentosTest.java
@@ -12,7 +12,7 @@ class CalculadoraDescuentosTest {
     void debeAplicarDescuentoDeAdultosMayores() {
         int descuento = calculadora.calcularDescuento(100, 70);
 
-        assertEquals(2111, descuento);
+        assertEquals(20667, descuento);
     }
 
     @Test
